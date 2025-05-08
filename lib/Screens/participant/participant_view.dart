68cce948ec197c13ab10e0806bc00fdb4d2eff36
veasnapovsr <veasnavarapov@gmail.com>
2025-05-08 21:00:40 +0700

    Update participant_view.dart

--- lib/Screens/participant/participant_view.dart
+++ lib/Screens/participant/participant_view.dart
@@ -6,7 +6,7 @@ import '../../models/participant_model.dart';
 class ParticipantView extends StatelessWidget {
   final bool isDarkMode;
   final VoidCallback toggleTheme;
-
+//main view for displaying participants.
   const ParticipantView({
     super.key,
     required this.isDarkMode,
